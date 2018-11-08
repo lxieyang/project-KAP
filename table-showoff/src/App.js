c54d78b622364b865bfe662a3443b28321e0175d
lxieyang <lxieyang@umich.edu>
2018-11-08 11:12:10 -0500

    embed google analytics to the table-showoff

--- table-showoff/src/App.js
+++ table-showoff/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Mainpage from '../../shared-components/src/containers/Mainpage/ShowOffMainpage';
+import ReactGA from 'react-ga';
 import styles from './App.css';
+
+ReactGA.initialize('UA-128939148-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   render () {
