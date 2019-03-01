8ad0387799525138e758533bc404146fefe7df5d
lxieyang <lxieyang@umich.edu>
2019-02-28 21:54:23 -0500

    fix web-hosted login welcome card style

--- shared-components/src/containers/Mainpage/containers/Auth/LoginPage/LoginPage.js
+++ shared-components/src/containers/Mainpage/containers/Auth/LoginPage/LoginPage.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 import firebase from '../../../../../firebase/firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { APP_NAME_SHORT } from '../../../../../shared/constants';
+import { googleIcon } from '../../../../../shared/utilities';
 import Logo from '../../../../../components/UI/Logo/Logo';
+
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import styles from './LoginPage.css';
+
+const materialStyles = {
+  card: {
+    maxWidth: 400
+  },
+  media: {
+    height: 140
+  }
+};
 
 class LoginPage extends Component {
   // The component's Local state.
   state = {
     isSignedIn: false, // Local signed-in state.
     user: null
-  };
-
-  // Configure FirebaseUI.
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false
-    }
   };
 
   // Listen to the Firebase Auth state and set the local state.
@@ -39,44 +45,63 @@ class LoginPage extends Component {
     this.unregisterAuthObserver();
   }
 
+  logInClickedHandler = () => {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {})
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
   render() {
+    const { classes } = this.props;
+
     if (!this.state.isSignedIn) {
       return (
         <div className={styles.LoginPage}>
-          <div className={styles.WelcomeCardContainer}>
-            <div className={styles.WelcomeCard}>
-              <div
-                style={{
-                  textAlign: 'center',
-                  fontSize: '1.3rem',
-                  fontWeight: '600'
-                }}
-              >
-                Welcome to {APP_NAME_SHORT}!
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: '10px'
-                }}
-              >
-                <Logo size={'30px'} />
-              </div>
-              <br />
-              <div>
-                <strong>{APP_NAME_SHORT}</strong> helps you collect and organize
-                online resources into meaningful structures while programming.
-                <br />
-                Please sign in with your Google account.
-              </div>
-            </div>
+          <div
+            style={{
+              width: '100%',
+              height: '350px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="h2"
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <Logo size={'28px'} /> &nbsp; Welcome to {APP_NAME_SHORT}!
+                </Typography>
+                <Typography component="p">
+                  <strong>{APP_NAME_SHORT}</strong> helps you collect and
+                  organize online resources into meaningful structures while
+                  programming.
+                </Typography>
+                <Typography
+                  component="p"
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  Please log in with your Google account.
+                </Typography>
+              </CardContent>
+              <CardContent>
+                <Chip
+                  avatar={<Avatar alt="G" src={googleIcon} />}
+                  label="Log in with Google"
+                  onClick={() => this.logInClickedHandler()}
+                />
+              </CardContent>
+            </Card>
           </div>
-          <StyledFirebaseAuth
-            uiConfig={this.uiConfig}
-            firebaseAuth={firebase.auth()}
-          />
         </div>
       );
     }
@@ -84,4 +109,4 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+export default withStyles(materialStyles)(LoginPage);
